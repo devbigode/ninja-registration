d54e4448e7dbc913c99f65c9f0e88c25a184121b
Guilherme Morais <Guilherme>
2025-04-02 22:29:32 -0300

    Criação da primeira rota

--- src/main/java/dev/java10x/CadastroNinja/Controller.java
+++ src/main/java/dev/java10x/CadastroNinja/Controller.java
@@ -1,4 +1,16 @@
 package dev.java10x.CadastroNinja;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+
+    @GetMapping("/")
+    public String boasVindas(){
+        return "<h1>Meu primeiro hello world com Spring</h1>";
+    }
 }
