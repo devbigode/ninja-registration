3e1e686ce53849de325047e42c1beee4d2ff8988
Dev <guilherme.ads@proton.me>
2025-04-04 16:11:43 -0300

    Utilizando recursos do Lombok

--- src/main/java/dev/java10x/NinjaRegistration/Mission/MissionModel.java
+++ src/main/java/dev/java10x/NinjaRegistration/Mission/MissionModel.java
@@ -2,26 +2,29 @@ package dev.java10x.NinjaRegistration.Mission;
 
 import dev.java10x.NinjaRegistration.Ninja.NinjaModel;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_mission")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class MissionModel{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String difficulty;
 
     @OneToMany(mappedBy = "mission")
+    @NonNull
     private List<NinjaModel> ninjaList;
-
-    public MissionModel(){}
-
-    public MissionModel(String name, String difficulty) {
-        this.name = name;
-        this.difficulty = difficulty;
-    }
 }
--- src/main/java/dev/java10x/NinjaRegistration/Ninja/NinjaModel.java
+++ src/main/java/dev/java10x/NinjaRegistration/Ninja/NinjaModel.java
@@ -2,34 +2,30 @@ package dev.java10x.NinjaRegistration.Ninja;
 
 import dev.java10x.NinjaRegistration.Mission.MissionModel;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "tb_register" )
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String name;
+    @NonNull
     private String email;
+    @NonNull
     private int age;
 
     @ManyToOne
     @JoinColumn(name = "id_mission") // Foreign Key
+    @NonNull
     private MissionModel mission;
-
-    public NinjaModel(){}
-
-    public NinjaModel(String name, String email, int age){
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public NinjaModel(String name, String email, int age, MissionModel mission) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-        this.mission = mission;
-    }
 }
